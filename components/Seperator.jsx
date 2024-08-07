da4ecfc7b5075a504815e6eb602cd6a4d9cc1d58
Alexandre Angrignon <alex040892@gmail.com>
2024-08-07 14:54:23 +1200

    Hero

---
+++ components/Seperator.jsx
@@ -0,0 +1,17 @@
+'use client';
+
+import Image from 'next/image';
+
+const Seperator = ({ bg = 'white' }) => {
+  const imgSrc =
+    bg === 'accent'
+      ? '/assets/separator-accent.svg'
+      : '/assets/separator-white.svg';
+  return (
+    <div className='relative w-[168px] h-[26px] mx-auto'>
+      <Image src={imgSrc} fill alt='' />
+    </div>
+  );
+};
+
+export default Seperator;
